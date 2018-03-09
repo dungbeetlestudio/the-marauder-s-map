6d8c729335c45ba08336cae9fdb4ff32fadd86e5
sltruman <sl.truman@live.com>
2018-03-09 17:03:25 +0800

    登陆页面

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Events, LoadingController, NavController, ViewController, NavParams } from 'ionic-angular';
+import { Events, NavController } from 'ionic-angular';
 import * as $ from "jquery";
 
 declare const AMap
@@ -14,7 +14,6 @@ export class HomePage {
 
   constructor(public events: Events,
     public navCtrl: NavController,
-    public loadingCtrl: LoadingController,
   ) {
     events.subscribe('appExit', () => {
       $('.step-exit').css('display', 'flex')
@@ -26,7 +25,39 @@ export class HomePage {
       resizeEnable: true
     })
 
-    $('.phone-numbers').focus()
+    //按钮事件
+    $('.step-1 .next')[0].onclick = this.next
+
+    $('.step-3 .code')[0].onfocus = () => $('.step-3 .code')[0].value = ''
+    $('.step-3 .code')[1].onfocus = () => $('.step-3 .code')[1].value = ''
+    $('.step-3 .code')[2].onfocus = () => $('.step-3 .code')[2].value = ''
+    $('.step-3 .code')[3].onfocus = () => $('.step-3 .code')[3].value = ''
+
+    $('.step-3 .code')[0].onkeyup = () => {
+      if ($('.step-3 .code')[0].value.length > 0)
+        $('.step-3 .code')[1].focus()
+    }
+
+    $('.step-3 .code')[1].onkeyup = () => {
+      if ($('.step-3 .code')[1].value.length > 0)
+        $('.step-3 .code')[2].focus()
+      else
+        $('.step-3 .code')[0].focus()
+    }
+
+    $('.step-3 .code')[2].onkeyup = () => {
+      if ($('.step-3 .code')[2].value.length > 0)
+        $('.step-3 .code')[3].focus()
+      else
+        $('.step-3 .code')[1].focus()
+    }
+
+    $('.step-3 .code')[3].onkeyup = () => {
+      if ($('.step-3 .code')[3].value.length > 0)
+        $('.step-3').css('display', 'none'), this.signIn()
+      else
+        $('.step-3 .code')[2].focus()
+    }
   }
 
   next() {
@@ -42,14 +73,26 @@ export class HomePage {
         clearInterval(timer)
         $('.step-2').css('display', 'none')
         $('.step-3').css('display', 'flex')
-
-        $('.step-3 .code')[0].onkeyup = () => { $('.step-3 .code')[1].focus() }
-        $('.step-3 .code')[1].onkeyup = () => { $('.step-3 .code')[2].focus() }
-        $('.step-3 .code')[2].onkeyup = () => { $('.step-3 .code')[3].focus() }
-        $('.step-3 .code')[3].onkeyup = () => { $('.sign-in').css('display', 'none') }
-        $('.step-3 .code')[0].focus()
       }
     }, 1000)
+  }
 
+  signIn() {
+    let timer
+    $('.step-4').show('fast', () => {
+      timer = setInterval(() => {
+        if ($('.step-4 .hint')[0].innerHTML.length > 10)
+          $('.step-4 .hint')[0].innerHTML = 'signing'
+        else
+          $('.step-4 .hint')[0].innerHTML += '.'
+      }, 1000)
+    })
+
+    // if (true) {
+    //   $('.step-4').hide('fast', () => {
+    //     $('.step-4 .hint').value = 'signing'
+    //     clearInterval(timer)
+    //   })
+    // }
   }
 }
